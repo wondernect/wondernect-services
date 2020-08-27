da591eaac92e17e69ed9d3a1816bede79ff52e42
530525 <530525@nd.com>
2020-08-27 18:16:48 +0800

    init

---
+++ ums-file/src/main/java/com/wondernect/services/ums/file/context/AuthorizeUserRoleContext.java
@@ -0,0 +1,16 @@
+package com.wondernect.services.ums.file.context;
+
+import com.wondernect.elements.authorize.context.impl.AbstractWondernectAuthorizeContext;
+import org.springframework.stereotype.Component;
+
+/**
+ * Copyright (C), 2017-2020, wondernect.com
+ * FileName: AuthorizeUserRoleContext
+ * Author: chenxun
+ * Date: 2020/8/27 16:56
+ * Description:
+ */
+@Component
+public class AuthorizeUserRoleContext extends AbstractWondernectAuthorizeContext {
+
+}
