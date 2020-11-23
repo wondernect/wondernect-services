f48d3ef0b2deec0a3c560d6fde747124e63000c9
530525 <530525@nd.com>
2020-11-23 14:26:31 +0800

    【m】新增bean property接口获取

--- ums-office/src/main/java/com/wondernect/services/ums/office/controller/excel/ExcelBeanPropertyController.java
+++ ums-office/src/main/java/com/wondernect/services/ums/office/controller/excel/ExcelBeanPropertyController.java
@@ -73,6 +73,16 @@ public class ExcelBeanPropertyController {
     }
 
     @AuthorizeUserRole(authorizeType = AuthorizeType.EXPIRES_TOKEN, authorizeRoleType = AuthorizeRoleType.ONLY_AUTHORIZE)
+    @ApiOperation(value = "获取详细信息", httpMethod = "GET")
+    @GetMapping(value = "/detail_by_bean_id_and_name")
+    public BusinessData<ExcelBeanPropertyResponseDTO> detailByBeanIdAndName(
+            @ApiParam(required = true) @NotBlank(message = "实体类id不能为空") @RequestParam(value = "beanId", required = false) String beanId,
+            @ApiParam(required = true) @NotBlank(message = "属性名不能为空") @RequestParam(value = "name", required = false) String name
+    ) {
+        return excelBeanPropertyFeignClient.detailByBeanIdAndName(beanId, name);
+    }
+
+    @AuthorizeUserRole(authorizeType = AuthorizeType.EXPIRES_TOKEN, authorizeRoleType = AuthorizeRoleType.ONLY_AUTHORIZE)
     @ApiOperation(value = "列表", httpMethod = "POST")
     @PostMapping(value = "/list")
     public BusinessData<List<ExcelBeanPropertyResponseDTO>> list(
