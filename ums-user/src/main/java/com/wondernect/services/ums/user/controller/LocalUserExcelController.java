fe40be20b567853595a3241de55701463df0dab7
chenxun <sunbeamhome@163.com>
2020-11-26 22:15:45 +0800

    【m】ums user添加本地用户导入导出服务

--- ums-user/src/main/java/com/wondernect/services/ums/user/controller/LocalUserExcelController.java
+++ ums-user/src/main/java/com/wondernect/services/ums/user/controller/LocalUserExcelController.java
@@ -3,10 +3,13 @@ package com.wondernect.services.ums.user.controller;
 import com.wondernect.elements.authorize.context.interceptor.AuthorizeRoleType;
 import com.wondernect.elements.authorize.context.interceptor.AuthorizeType;
 import com.wondernect.elements.authorize.context.interceptor.AuthorizeUserRole;
+import com.wondernect.elements.common.error.BusinessError;
 import com.wondernect.elements.common.response.BusinessData;
+import com.wondernect.elements.common.utils.ESObjectUtils;
+import com.wondernect.services.ums.user.service.LocalUserExcelExportService;
+import com.wondernect.services.ums.user.service.LocalUserExcelImportService;
+import com.wondernect.services.ums.user.service.LocalUserExcelInitService;
 import com.wondernect.stars.user.dto.ListUserRequestDTO;
-import com.wondernect.stars.user.feign.user.LocalUserExcelFeignClient;
-import feign.Response;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
@@ -22,9 +25,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
 
 /**
  * Copyright (C), 2017-2019, wondernect.com
@@ -42,7 +42,13 @@ public class LocalUserExcelController {
     private static final Logger logger = LoggerFactory.getLogger(LocalUserExcelController.class);
 
     @Autowired
-    private LocalUserExcelFeignClient localUserExcelFeignClient;
+    private LocalUserExcelInitService localUserExcelInitService;
+
+    @Autowired
+    private LocalUserExcelExportService localUserExcelExportService;
+
+    @Autowired
+    private LocalUserExcelImportService localUserExcelImportService;
 
     @AuthorizeUserRole(authorizeType = AuthorizeType.EXPIRES_TOKEN, authorizeRoleType = AuthorizeRoleType.ONLY_AUTHORIZE)
     @ApiOperation(value = "初始化本地用户导入导出item", httpMethod = "POST")
@@ -50,66 +56,79 @@ public class LocalUserExcelController {
     public BusinessData initLocalUserExcelItem(
             @ApiParam(required = false) @RequestParam(value = "force_update", required = false) Boolean forceUpdate
     ) {
-        return localUserExcelFeignClient.initLocalUserExcelItem(forceUpdate);
+        if (ESObjectUtils.isNull(forceUpdate)) {
+            forceUpdate = false;
+        }
+        localUserExcelInitService.initLocalUserExcelItem(forceUpdate);
+        return new BusinessData(BusinessError.SUCCESS);
     }
 
     @AuthorizeUserRole(authorizeType = AuthorizeType.EXPIRES_TOKEN, authorizeRoleType = AuthorizeRoleType.ONLY_AUTHORIZE)
     @ApiOperation(value = "本地用户导出", httpMethod = "POST")
-    @PostMapping(value = "/excel_data_export")
+    @PostMapping(value = "/local_user_data_export")
     public void excelDataExport(
             @ApiParam(required = true) @NotBlank(message = "模板id不能为空") @RequestParam(value = "template_id", required = false) String templateId,
             @ApiParam(required = true) @NotNull(message = "列表请求参数不能为空") @Validated @RequestBody(required = false) ListUserRequestDTO listUserRequestDTO,
             HttpServletRequest request,
             HttpServletResponse response
     ) {
-        Response feignResponse = localUserExcelFeignClient.excelDataExport(templateId, listUserRequestDTO);
-        exportExcel(feignResponse, "本地用户信息.xlsx", request, response);
+        try {
+            localUserExcelExportService.excelDataExport(templateId, listUserRequestDTO, request, response);
+        } catch (Exception e) {
+            BusinessData.error(e.getMessage(), response);
+        }
     }
 
     @AuthorizeUserRole(authorizeType = AuthorizeType.EXPIRES_TOKEN, authorizeRoleType = AuthorizeRoleType.ONLY_AUTHORIZE)
     @ApiOperation(value = "本地用户导入", httpMethod = "POST")
-    @PostMapping(value = "/excel_data_import", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @PostMapping(value = "/local_user_data_import", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public void excelDataImport(
             @ApiParam(required = true) @NotBlank(message = "模板id不能为空") @RequestParam(value = "template_id", required = false) String templateId,
-            @ApiParam(required = true) @RequestPart(value = "file", required = false) MultipartFile file,
+            @ApiParam(required = true) @NotNull(message = "导入文件不能为空") @RequestPart(value = "file", required = false) MultipartFile file,
             HttpServletRequest request,
             HttpServletResponse response
     ) {
-        Response feignResponse = localUserExcelFeignClient.excelDataImport(templateId, file);
-        exportExcel(feignResponse, "本地用户信息导入失败.xlsx", request, response);
+        try {
+            localUserExcelImportService.excelDataImport(templateId, file.getInputStream(), request, response);
+        } catch (Exception e) {
+            BusinessData.error(e.getMessage(), response);
+        }
     }
 
     @AuthorizeUserRole(authorizeType = AuthorizeType.EXPIRES_TOKEN, authorizeRoleType = AuthorizeRoleType.ONLY_AUTHORIZE)
     @ApiOperation(value = "本地用户导入模板下载", httpMethod = "GET")
-    @GetMapping(value = "/excel_data_import_model")
+    @GetMapping(value = "/local_user_data_import_model")
     public void excelDataImportModel(
             @ApiParam(required = true) @NotBlank(message = "模板id不能为空") @RequestParam(value = "template_id", required = false) String templateId,
             HttpServletRequest request,
             HttpServletResponse response
     ) {
-        Response feignResponse = localUserExcelFeignClient.excelDataImportModel(templateId);
-        exportExcel(feignResponse, templateId + ".xlsx", request, response);
+        try {
+            localUserExcelExportService.excelDataImportModel(templateId, request, response);
+        } catch (Exception e) {
+            BusinessData.error(e.getMessage(), response);
+        }
     }
 
     /**
      * 导出文件(请求响应)
      */
-    private void exportExcel(Response feignResponse, String fileName, HttpServletRequest request, HttpServletResponse response) {
-        try {
-            InputStream inputStream = feignResponse.body().asInputStream();
-            fileName = new String(fileName.getBytes(StandardCharsets.UTF_8), StandardCharsets.ISO_8859_1);
-            response.setHeader("content-type", "application/vnd.ms-excel");
-            response.setHeader("content-disposition", "attachment;filename=" + fileName);
-            byte[] bytes = new byte[1024];
-            int len;
-            while ((len = inputStream.read(bytes)) != -1) {
-                response.getOutputStream().write(bytes, 0, len);
-            }
-            inputStream.close();
-            response.getOutputStream().close();
-            response.getOutputStream().flush();
-        } catch (IOException e) {
-            logger.error("Excel export failed, message={}, stacktrace={}", e.getLocalizedMessage(), e.getCause());
-        }
-    }
+    // private void exportExcel(Response feignResponse, String fileName, HttpServletRequest request, HttpServletResponse response) {
+    //     try {
+    //         InputStream inputStream = feignResponse.body().asInputStream();
+    //         fileName = new String(fileName.getBytes(StandardCharsets.UTF_8), StandardCharsets.ISO_8859_1);
+    //         response.setHeader("content-type", "application/vnd.ms-excel");
+    //         response.setHeader("content-disposition", "attachment;filename=" + fileName);
+    //         byte[] bytes = new byte[1024];
+    //         int len;
+    //         while ((len = inputStream.read(bytes)) != -1) {
+    //             response.getOutputStream().write(bytes, 0, len);
+    //         }
+    //         inputStream.close();
+    //         response.getOutputStream().close();
+    //         response.getOutputStream().flush();
+    //     } catch (IOException e) {
+    //         logger.error("Excel export failed, message={}, stacktrace={}", e.getLocalizedMessage(), e.getCause());
+    //     }
+    // }
 }
