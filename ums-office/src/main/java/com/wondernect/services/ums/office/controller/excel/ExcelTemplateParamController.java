4ed37f84417561245d55478591dcb2882032ae9e
530525 <530525@nd.com>
2020-12-03 14:36:52 +0800

    【m】新增模板参数批量添加接口

--- ums-office/src/main/java/com/wondernect/services/ums/office/controller/excel/ExcelTemplateParamController.java
+++ ums-office/src/main/java/com/wondernect/services/ums/office/controller/excel/ExcelTemplateParamController.java
@@ -5,10 +5,7 @@ import com.wondernect.elements.authorize.context.interceptor.AuthorizeType;
 import com.wondernect.elements.authorize.context.interceptor.AuthorizeUserRole;
 import com.wondernect.elements.common.response.BusinessData;
 import com.wondernect.elements.rdb.response.PageResponseData;
-import com.wondernect.stars.office.excel.dto.param.ExcelTemplateParamResponseDTO;
-import com.wondernect.stars.office.excel.dto.param.ListExcelTemplateParamRequestDTO;
-import com.wondernect.stars.office.excel.dto.param.PageExcelTemplateParamRequestDTO;
-import com.wondernect.stars.office.excel.dto.param.SaveExcelTemplateParamRequestDTO;
+import com.wondernect.stars.office.excel.dto.param.*;
 import com.wondernect.stars.office.feign.excel.param.ExcelTemplateParamFeignClient;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -34,6 +31,15 @@ public class ExcelTemplateParamController {
 
     @Autowired
     private ExcelTemplateParamFeignClient excelTemplateParamFeignClient;
+
+    @AuthorizeUserRole(authorizeType = AuthorizeType.EXPIRES_TOKEN, authorizeRoleType = AuthorizeRoleType.ONLY_AUTHORIZE)
+    @ApiOperation(value = "批量添加", httpMethod = "POST")
+    @PostMapping(value = "/batch_add")
+    public BusinessData batchAdd(
+            @ApiParam(required = true) @NotNull(message = "请求参数不能为空") @Validated @RequestBody(required = false) BatchAddExcelTemplateParamRequestDTO batchAddExcelTemplateParamRequestDTO
+    ) {
+        return excelTemplateParamFeignClient.batchAdd(batchAddExcelTemplateParamRequestDTO);
+    }
 
     @AuthorizeUserRole(authorizeType = AuthorizeType.EXPIRES_TOKEN, authorizeRoleType = AuthorizeRoleType.ONLY_AUTHORIZE)
     @ApiOperation(value = "创建", httpMethod = "POST")
